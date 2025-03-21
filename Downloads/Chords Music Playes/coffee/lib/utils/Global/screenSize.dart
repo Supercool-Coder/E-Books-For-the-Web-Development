80a51b5a109c9cb6d11321ffbe9c03f2ec6f1093
Supercool Coder <uttampbh123@gmail.com>
2025-03-21 12:51:07 +0530

    Reset main branch

--- Downloads/Chords Music Playes/coffee/lib/utils/Global/screenSize.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ScreenUtils {
-  static double getWidth(BuildContext context) =>
-      MediaQuery.of(context).size.width;
-  static double getHeight(BuildContext context) =>
-      MediaQuery.of(context).size.height;
-}
